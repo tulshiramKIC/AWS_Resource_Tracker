2af4402e8b6745f790288b91d951c5e48bfaf374
Tdk785 <108582568+Tdk785@users.noreply.github.com>
2024-01-17 00:36:33 +0530

    Create aws_resource_tracker.sh

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+################################################
+# Author: Tulshiram
+# Date: 11th-Jan
+#
+# Version: v1
+#
+# This script will report the AWS resource usage
+################################################
+
+
+#For below command just refer AWS CLI Reference Commands
+
+set -x
+
+# AWS S3
+echo "Listing S3 buckets:"
+aws s3 ls
+
+# AWS EC2
+echo "Listing EC2 Instances:"
+aws ec2 describe-instances
+
+# AWS Lambda
+echo "Listing Lambda functions:"
+aws lambda list-functions
+
+# AWS IAM Users
+echo "Listing IAM users:"
+aws iam list-users
